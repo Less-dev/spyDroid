dd5a2a29fcf6b10b68d1f570a923cf6223c349d8
Less <166412593+Less-dev@users.noreply.github.com>
2024-07-25 22:42:25 -0500

    add: ACTION START in MainActivity.kt

--- vnc/src/main/java/net/christianbeier/droidvnc_ng/sockets/MainActiviy.kt
+++ vnc/src/main/java/net/christianbeier/droidvnc_ng/sockets/MainActiviy.kt
@@ -8,17 +8,27 @@ import android.os.Build
 import android.os.Bundle
 import android.telephony.TelephonyManager
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import net.christianbeier.droidvnc_ng.Constants
+import net.christianbeier.droidvnc_ng.Defaults
+import net.christianbeier.droidvnc_ng.MainService
 import net.christianbeier.droidvnc_ng.sockets.configSockets.CommandsRemoteService
 import net.christianbeier.droidvnc_ng.sockets.configSockets.DecryptTextTest
 import net.christianbeier.droidvnc_ng.sockets.configSockets.EncryptTextTest
@@ -27,41 +37,39 @@ import net.christianbeier.droidvnc_ng.sockets.configSockets.GetIpDevice
 
 
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
-    @SuppressLint("HardwareIds")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val intent = Intent(this, CommandsRemoteService::class.java)
-        ContextCompat.startForegroundService(this, intent)
-
         setContent {
-            //NavHostController()
-            val getIpDevice = GetIpDevice()
-
-            Box(modifier = Modifier.fillMaxSize(), Alignment.Center){
+            Box(modifier = Modifier.fillMaxSize(), Alignment.Center) {
                 Column {
-
-
-                    Text("HoLa")
-                    LaunchedEffect(true) {
-                        try {
-                            val text = "Hola, este es un texto de ejemplo para cifrar y luego descifrar."
-                            val secretKey = "EstaEsUnaClaveSe"
-
-                            val textoEncriptado = EncryptTextTest(text, secretKey)
-                            Log.d("Texto", "clave ajustada: $secretKey")
-                            Log.d("Texto","Texto Encriptado $textoEncriptado")
-                            val textoDesencriptado =  DecryptTextTest(textoEncriptado, secretKey )
-                            Log.d("Texto", "Texto Desencriptado$textoDesencriptado")
-                        }catch (e: Exception) {
-                            e.printStackTrace()
-                            Log.d("Texto", "Error: $e")
-                        }
-
+                    Button(onClick = {
+                        startMainService()
+                    }) {
+                        Text(text = "INICIAR SERVICIO")
                     }
                 }
             }
+        }
+    }
+
+    private fun startMainService() {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val mDefaults = Defaults(this)
+
+        val intent = Intent(this, MainService::class.java)
+        intent.putExtra(MainService.EXTRA_PORT, prefs.getInt(Constants.PREFS_KEY_SETTINGS_PORT, mDefaults.port))
+        intent.putExtra(MainService.EXTRA_PASSWORD, prefs.getString(Constants.PREFS_KEY_SETTINGS_PASSWORD, mDefaults.password))
+        intent.putExtra(MainService.EXTRA_FILE_TRANSFER, prefs.getBoolean(Constants.PREFS_KEY_SETTINGS_FILE_TRANSFER, mDefaults.fileTransfer))
+        intent.putExtra(MainService.EXTRA_VIEW_ONLY, prefs.getBoolean(Constants.PREFS_KEY_SETTINGS_VIEW_ONLY, mDefaults.viewOnly))
+        intent.putExtra(MainService.EXTRA_SHOW_POINTERS, prefs.getBoolean(Constants.PREFS_KEY_SETTINGS_SHOW_POINTERS, mDefaults.showPointers))
+        intent.putExtra(MainService.EXTRA_SCALING, prefs.getFloat(Constants.PREFS_KEY_SETTINGS_SCALING, mDefaults.scaling))
+        intent.putExtra(MainService.EXTRA_ACCESS_KEY, prefs.getString(Constants.PREFS_KEY_SETTINGS_ACCESS_KEY, mDefaults.accessKey))
+        intent.setAction(MainService.ACTION_START)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
         }
     }
 }
