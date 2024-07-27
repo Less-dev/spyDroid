2ab3db827e7581a0561d0cc2c6c0b7c80cc2841b
Less <166412593+Less-dev@users.noreply.github.com>
2024-07-26 20:00:37 -0500

    add: jitpack.io to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
         mavenCentral()
+        maven (url = "https://jitpack.io" )
     }
     dependencies {
         classpath(libs.gradle)
@@ -14,6 +14,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven (url = "https://jitpack.io" )
     }
 }
 
