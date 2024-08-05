47e1e0612a7815d70fd39453831d96f8c5bc72d8
Less <166412593+Less-dev@users.noreply.github.com>
2024-08-04 21:18:49 -0500

    add: add get private ip address in globalViewModel

--- core-data/src/main/java/net/spydroid/core/data/common/GlobalViewModel.kt
+++ core-data/src/main/java/net/spydroid/core/data/common/GlobalViewModel.kt
@@ -17,6 +17,12 @@
 
 package net.spydroid.core.data.common
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
+import android.os.Build
 import androidx.compose.runtime.compositionLocalOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -26,6 +32,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import net.spydroid.core.data.domain.PreferenceManagerRepository
+import java.net.InetAddress
+import java.net.NetworkInterface
+import java.util.Enumeration
 import javax.inject.Inject
 
 
@@ -39,9 +48,38 @@ class GlobalViewModel  @Inject constructor(
     private val _stateVncServer = MutableStateFlow(preferenceManagerRepository.getData("state"))
     val stateVncServer: StateFlow<Boolean> = _stateVncServer
 
+    private val _privateIpAddress = MutableStateFlow("")
+    val privateIpAddress: StateFlow<String> = _privateIpAddress
+
     fun changeValueVncServer(value: Boolean) =
         viewModelScope.launch(Dispatchers.IO) {
             preferenceManagerRepository.saveData(key = "state", value = value)
             _stateVncServer.value = value
         }
+
+    fun get_private_ip_address() {
+        _privateIpAddress.value = getPrivateIPAddress() ?: ""
+    }
+
+    private fun getPrivateIPAddress(): String? {
+        try {
+            val interfaces = NetworkInterface.getNetworkInterfaces()
+            while (interfaces.hasMoreElements()) {
+                val networkInterface = interfaces.nextElement()
+                val addresses = networkInterface.inetAddresses
+                while (addresses.hasMoreElements()) {
+                    val inetAddress = addresses.nextElement()
+                    if (!inetAddress.isLoopbackAddress && inetAddress is InetAddress) {
+                        val ip = inetAddress.hostAddress
+                        if (inetAddress.isSiteLocalAddress) {
+                            return ip
+                        }
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
 }
