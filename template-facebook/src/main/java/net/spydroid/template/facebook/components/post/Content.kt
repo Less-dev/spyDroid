639353f3e73eea503834c991ed6b3b13f6123ebb
Less <166412593+Less-dev@users.noreply.github.com>
2024-08-07 17:40:25 -0500

    add: apk release v0.0.1-alpha

--- template-facebook/src/main/java/net/spydroid/template/facebook/components/post/Content.kt
+++ template-facebook/src/main/java/net/spydroid/template/facebook/components/post/Content.kt
@@ -26,8 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import net.spydroid.template.facebook.R
-import net.spydroid.template.facebook.models.POST
+import net.spydroid.core.ui.R
 
 @Composable
 internal fun ContentPost(image: Int? = null, onClicContentPost: () -> Unit) {
--- template-facebook/src/main/java/net/spydroid/template/facebook/components/whatYouThink/WhatDoYouThink.kt
+++ template-facebook/src/main/java/net/spydroid/template/facebook/components/whatYouThink/WhatDoYouThink.kt
@@ -25,7 +25,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import net.spydroid.template.facebook.R
+import net.spydroid.core.ui.R
 
 @Composable
 internal fun WhatDoYouThink(
