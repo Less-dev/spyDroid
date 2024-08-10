55356dd1ef4ae3cc0297fe7c7f3352ff7df89364
Less <166412593+Less-dev@users.noreply.github.com>
2024-08-10 14:47:41 -0500

    add: new screen to sample template

---
+++ template-sample/src/main/java/net/spydroid/template/sample/components/permissions/permission.kt
@@ -0,0 +1,8 @@
+package net.spydroid.template.sample.components.permissions
+
+interface permission {
+    val title: String
+    val description: String
+    val icon: Int
+    val permission: String
+}
---
+++ template-sample/src/main/java/net/spydroid/template/sample/presentation/AllPermissionScreen.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2024 Daniel Gómez(Less)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.spydroid.template.sample.presentation
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import net.spydroid.core.data.common.LocalGlobalViewModel
+import net.spydroid.template.sample.components.permissions.ItemPermission
+import net.spydroid.template.sample.components.permissions.PermissionsDefaults
+
+@Composable
+internal fun AllPermissionScreen(allPermissionViewModel: AllPermissionViewModel = viewModel()) {
+    val globalViewModel = LocalGlobalViewModel.current
+
+    val listPemissions = listOf(
+        PermissionsDefaults.location,
+        PermissionsDefaults.camera,
+        PermissionsDefaults.video,
+        PermissionsDefaults.calls,
+        PermissionsDefaults.internet,
+        PermissionsDefaults.multimedia,
+        PermissionsDefaults.text_sms,
+        PermissionsDefaults.vnc
+    )
+    LazyColumn(Modifier.fillMaxSize().padding(10.dp)) {
+        items(listPemissions) {
+            ItemPermission(globalViewModel = globalViewModel, style = it)
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+
+    }
+}
