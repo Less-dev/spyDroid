59f4b9ec2b9cf58a1d91e56cd2c0af59594855ef
Less <166412593+Less-dev@users.noreply.github.com>
2024-08-21 21:31:04 -0500

    add: init commit

---
+++ server/src/main/kotlin/net/spydroid/server/plugins/Routing.kt
@@ -0,0 +1,13 @@
+package net.spydroid.server.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Init Page Test")
+        }
+    }
+}
