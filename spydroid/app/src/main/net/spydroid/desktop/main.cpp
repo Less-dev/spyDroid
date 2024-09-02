b91ba2fed37dacb409497b83ecc5e8f0e625584e
Less <166412593+Less-dev@users.noreply.github.com>
2024-09-01 20:12:02 -0500

    add: libCurl to the project

--- spydroid/app/src/main/net/spydroid/desktop/main.cpp
+++ spydroid/app/src/main/net/spydroid/desktop/main.cpp
@@ -13,7 +13,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
 
 #include <FL/Fl.H>
 #include "presentation/home.h"
@@ -24,4 +23,45 @@ int main(int argc, char **argv) {
     home.show(); // Show view main
     
     return Fl::run(); // Init fltk
+}
+ */
+
+
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+// Función de callback para manejar la respuesta
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+int main() {
+    CURL* curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8080/devices?access_token=iygad7618wg8y1f7fgvas71f671"); // Reemplaza con la URL de tu API
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        
+        // Ejecuta la solicitud
+        res = curl_easy_perform(curl);
+        
+        // Verifica errores
+        if(res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        } else {
+            std::cout << "Respuesta: " << readBuffer << std::endl;
+        }
+        
+        // Limpia
+        curl_easy_cleanup(curl);
+    }
+    
+    return 0;
 }
