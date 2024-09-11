10cefe1251ede6979dc45973f42662f73a864391
Less <166412593+Less-dev@users.noreply.github.com>
2024-09-11 14:50:28 -0500

    add: gitignore

--- build/CMakeFiles/spydroid.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for spydroid.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/spydroid.dir/qrc_resources.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/spydroid.dir/qrc_resources.o: \
- /home/less/SPYDROID/c2-desktop/build/qrc_resources.cpp \
- /usr/include/stdc-predef.h
